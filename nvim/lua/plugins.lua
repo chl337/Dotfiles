0c370eed0f60687c21c3a22617c572c5958a279d
chl337 <christob.tr@hotmail.com>
2024-01-22 13:29:45 +0100

    Dotfiles: append LSP for rust.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -42,6 +42,9 @@ return {
             -- Snippets
             {'L3MON4D3/LuaSnip'},             -- Required
             {'rafamadriz/friendly-snippets'}, -- Optional
+
+            -- Rust
+            {'simrat39/rust-tools.nvim'},     -- Rust LSP
         }
     },
 --    -- TODO
